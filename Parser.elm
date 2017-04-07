20be82f056697fffc27bd3555c19c8f4c9be086a
xarvh <francesco.orsenigo@gmail.com>
2017-04-08 09:51:28 +1000

    Rewrite to calc

---
+++ Parser.elm
@@ -0,0 +1,85 @@
+module Parser exposing (..)
+
+import Combine exposing (..)
+import Combine.Num
+
+
+type Node
+    = Element String
+    | FunctionCall Node (List Node) -- function, arguments
+
+
+test =
+    FunctionCall
+        (Element "+")
+        [ Element "2"
+        , FunctionCall
+            (Element "*")
+            [ Element "4"
+            , Element "2"
+            ]
+        ]
+
+
+integer =
+    Combine.Num.int
+        |> Combine.map (toString >> Element)
+
+
+operator =
+    Combine.regex "[~!=@#$%^&*-+|<>]+"
+        |> Combine.map Element
+
+
+symbol =
+    Combine.regex "[a-z][a-zA-Z0-9]*"
+        |> Combine.map Element
+
+
+element : Parser s Node
+element =
+    Combine.choice
+        [ integer
+        , Combine.parens operator
+        , symbol
+        ]
+
+
+atom : Parser s Node
+atom =
+    Combine.choice
+        [ element
+        , Combine.parens <| Combine.lazy <| \() -> expression
+        ]
+
+
+functionCall : Parser s Node
+functionCall =
+    let
+        listToParser list =
+            case list of
+                [] ->
+                    Combine.fail "nope"
+
+                element :: [] ->
+                    Combine.succeed element
+
+                function :: arguments ->
+                    Combine.succeed <| FunctionCall function arguments
+    in
+        Combine.sepBy1 Combine.whitespace atom
+            |> Combine.andThen listToParser
+
+
+expression : Parser s Node
+expression =
+    Combine.lazy <|
+        \() ->
+            Combine.choice
+                [ atom <* Combine.end
+                , functionCall
+                ]
+
+
+parse code =
+    Combine.parse (expression <* Combine.end) code
