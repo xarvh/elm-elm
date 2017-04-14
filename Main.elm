f9e811562a5e495c5f77cca7fe7f0fed333eecff
xarvh <francesco.orsenigo@gmail.com>
2017-04-14 17:52:41 +1000

    tree view

--- Main.elm
+++ Main.elm
@@ -1,9 +1,14 @@
 module Main exposing (..)
 
-import Parser
+import Combine
 import Html exposing (..)
 import Html.Attributes
 import Html.Events
+import Parser
+import Svg exposing (Svg)
+import Svg.Attributes
+import TreeDiagram
+import TreeDiagram.Svg
 
 
 type alias Model =
@@ -29,37 +34,71 @@ update msg model =
 
 
 -- VIEW
-{-
-   drawLine : ( Float, Float ) -> ( Float, Float ) -> S.Svg msg
-   drawLine ( sourceX, sourceY ) ( targetX, targetY ) =
-     S.line
-       [ SA.x1 <| toString sourceX
-       , SA.y1 <| toString sourceY
-       , SA.x2 <| toString targetX
-       , SA.y2 <| toString targetY
-       , SA.stroke "black" ]
-       []
 
 
-   drawNode : String -> S.Svg msg
-   drawNode value =
-     S.g
-       []
-       [ S.circle
-           [ SA.r "16", SA.stroke "black", SA.fill "white", SA.cx "0", SA.cy "0" ]
-           []
-       , S.text'
-           [ SA.transform "translate(-8 5)" ]
-           [ S.text value ]
-       ]
+drawLine : ( Float, Float ) -> Svg msg
+drawLine ( targetX, targetY ) =
+    Svg.line
+        [ Svg.Attributes.x1 <| toString 0
+        , Svg.Attributes.y1 <| toString 0
+        , Svg.Attributes.x2 <| toString targetX
+        , Svg.Attributes.y2 <| toString targetY
+        , Svg.Attributes.stroke "black"
+        ]
+        []
 
 
+drawNode : ( Combine.ParseLocation, String ) -> Svg msg
+drawNode ( location, label ) =
+    Svg.g
+        []
+        [ Svg.circle
+            [ Svg.Attributes.r "16"
+            , Svg.Attributes.stroke "black"
+            , Svg.Attributes.fill "white"
+            , Svg.Attributes.cx "0"
+            , Svg.Attributes.cy "0"
+            , Html.Attributes.alt (toString location)
+            ]
+            []
+        , Svg.text_
+            [ Svg.Attributes.transform "translate(-8 5)" ]
+            [ Svg.text label ]
+        ]
 
 
-   node2tree : Compiler.Node -> TreeDiagram.Tree String
-   node2tree (Compiler.Node {value, fragmentType, children}) =
-       TreeDiagram.node (value ++ ":" ++ toString fragmentType) <| List.map node2tree children
+{-| locatedExpressionToTree
 -}
+le2t : Parser.LocatedExpression -> TreeDiagram.Tree ( Combine.ParseLocation, String )
+le2t (Parser.LocatedExpression location expression) =
+    let
+        n =
+            TreeDiagram.node
+    in
+        case expression of
+            Parser.BinaryExpression left reference right ->
+                n ( location, reference ) [ le2t left, le2t right ]
+
+            Parser.FunctionCall function arguments ->
+                n (location, "call") (List.map le2t (function :: arguments))
+
+            Parser.ListExpression elements ->
+                n ( location, "[]" ) (List.map le2t elements)
+
+            Parser.LiteralExpression value ->
+                n ( location, value ) []
+
+            Parser.TupleExpression entries ->
+                n ( location, "()" ) (List.map le2t entries)
+
+            Parser.UnaryExpression operator value ->
+                n ( location, toString operator ) [ le2t value ]
+
+            Parser.Unit ->
+                n ( location, "unit" ) []
+
+            Parser.VariableExpression variableReference ->
+                n ( location, variableReference ) []
 
 
 view code =
@@ -86,8 +125,7 @@ view code =
                 [ text code ]
             , div
                 [ Html.Attributes.style
-                    [ ( "width", "700px" )
-                    , ( "margin-top", "2rem" )
+                    [ ( "margin-top", "2rem" )
                     ]
                 ]
                 [ case result of
@@ -106,7 +144,9 @@ view code =
                             ]
 
                     Ok ( _, _, tree ) ->
-                        div [] [ text <| toString tree ]
+                      tree
+                        |> le2t
+                        |> TreeDiagram.Svg.draw TreeDiagram.defaultTreeLayout drawNode drawLine 
                 ]
             ]
 
